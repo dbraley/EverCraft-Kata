507d45c5c74fcaebf16fb8911613c10320e21c98
David Braley <David.Braley@ithaka.org>
2015-05-12 11:47:05 -0400

    Add @Test getName

--- java/src/tests/java/avatars/CharacterTest.java
+++ java/src/tests/java/avatars/CharacterTest.java
@@ -1,16 +1,25 @@
 package avatars;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class CharacterTest {
     private Character character;
+
     @Before
     public void setUp() {
         character = new Character();
     }
+
     @Test
     public void canSetName() {
         character.setName("Dave");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Dave", character.getName());
     }
 }
